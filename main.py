ff287d885c90f7bb6cc33630ae6c78e17a2096c8
White_Dragon <mos098tafa@gmail.com>
2023-02-06 17:28:35 +0200

    video done with fix some errors

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
 import os
 import shutil
 path=os.path.dirname(os.path.realpath(__file__))
-Files=os.listdir(path)
+
 
 #sort files by extensions
-for file in Files:
+for file in os.listdir(path):
     if file.endswith((".mp3",".acc",".wav",".flac",".m4a")):
-        if "music" in Files:
+        if "music" in os.listdir(path):
             shutil.copy(file,"music")
             os.remove(file)
-        else:
+            print("done")
+            continue
+        elif "music" not in os.listdir(path) :
             os.mkdir("music")
             shutil.copy(file,"music")
             os.remove(file)
+            print("done")
+            continue
         #music files
     if file.endswith(("mp4",".mkv",".mov",".wmv","avi")):
-        pass
+        if "video" in os.listdir(path):
+            shutil.copy(file,"video")
+            os.remove(file)
+            print("done")
+            continue
+        elif "video" not in os.listdir(path):
+            os.mkdir("video")
+            shutil.copy(file,"video")
+            os.remove(file)
+            print("done")
+            continue
         #video files
     if file.endswith((".jpeg",".jpg",".png",".gif",".tiff")):
         pass
