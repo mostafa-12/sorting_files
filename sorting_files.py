20dbd037319a2d2c8c0a3da5c1055b3c7958f51b
White_Dragon <mos098tafa@gmail.com>
2023-02-07 04:11:56 +0200

    done for 100000 onece

--- sorting_files.py
+++ sorting_files.py
@@ -7,12 +7,12 @@ def run(file_into=os.getcwd(),output_path=os.getcwd()):
     os.chdir(path)
     for file in os.listdir(path):
         if file.endswith((".mp3",".acc",".wav",".flac",".m4a")):
-            if "music" in os.listdir(path):
+            if "music" in os.listdir(output_path):
                 shutil.copy(file,f"{output_path}/music")
                 os.remove(file)
                 print("done : ",file)
                 continue
-            elif "music" not in os.listdir(path) :
+            elif "music" not in os.listdir(output_path) :
                 os.mkdir(f"{output_path}/music")
                 shutil.copy(file,f"{output_path}/music")
                 os.remove(file)
@@ -20,12 +20,12 @@ def run(file_into=os.getcwd(),output_path=os.getcwd()):
                 continue
             #music files
         if file.endswith(("mp4",".mkv",".mov",".wmv","avi")):
-            if "video" in os.listdir(path):
+            if "video" in os.listdir(output_path):
                 shutil.copy(file,f"{output_path}/video")
                 os.remove(file)
                 print("done : ",file)
                 continue
-            elif "video" not in os.listdir(path):
+            elif "video" not in os.listdir(output_path):
                 os.mkdir(f"{output_path}/video")
                 shutil.copy(file,f"{output_path}/video")
                 os.remove(file)
@@ -33,12 +33,12 @@ def run(file_into=os.getcwd(),output_path=os.getcwd()):
                 continue
             #video files
         if file.endswith((".jpeg",".jpg",".png",".gif",".tiff")):
-            if "photo" in os.listdir(path):
+            if "photo" in os.listdir(output_path):
                 shutil.copy(file,f"{output_path}/photo")
                 os.remove(file)
                 print("done : ",file)
                 continue
-            elif "photo" not in os.listdir(path):
+            elif "photo" not in os.listdir(output_path):
                 os.mkdir(f"{output_path}/photo")
                 shutil.copy(file,f"{output_path}/photo")
                 os.remove(file)
@@ -46,12 +46,12 @@ def run(file_into=os.getcwd(),output_path=os.getcwd()):
                 continue
             #photo files
         if file.endswith((".pdf",".doc",".docx",".xls",".xlsx",".ppt",".pptx",".txt",".odt")):
-            if "documents" in os.listdir(path):
+            if "documents" in os.listdir(output_path):
                 shutil.copy(file,f"{output_path}/documents")
                 os.remove(file)
                 print("done : ",file)
                 continue
-            elif "documents" not in os.listdir(path):
+            elif "documents" not in os.listdir(output_path):
                 os.mkdir(f"{output_path}/documents")
                 shutil.copy(file,f"{output_path}/documents")
                 os.remove(file)
